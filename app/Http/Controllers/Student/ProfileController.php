4de1dd93bddac36e62e41995d39eecfee574bd0d
bagi <bagi@arch.ma>
2025-07-08 23:20:47 +0100

    add helper function/utils

--- app/Http/Controllers/Student/ProfileController.php
+++ app/Http/Controllers/Student/ProfileController.php
@@ -5,12 +5,20 @@ namespace App\Http\Controllers\Student;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use function App\Http\Controllers\Student\utils\get_auth_user;
+
 class ProfileController extends Controller
 {
     //
     public function index(Request $req)
     {
-        //
+        $user = get_auth_user();
+
+        if (! $user) {
+            abort(404, 'User not found or role mismatch');
+        }
+
+        $requests = $user->bookRequests;
 
     }
 }
