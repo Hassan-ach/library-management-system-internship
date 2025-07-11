a8c32b82709607687438c16c96a2e5cd39f5e812
bagi <bagi@arch.ma>
2025-07-11 17:52:11 +0100

    add some librarian actions/ show->studentProfile and processe->to processe a bookReq / change get_borrowed_copies function in helpers / add latestRequestInfo relationship to BookRequest model

---
+++ app/Http/Controllers/Librarian/RequestController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\Librarian;
+
+use App\Enums\RequestStatus;
+use App\Http\Controllers\Controller;
+use App\Models\BookRequest;
+use App\Models\RequestInfo;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Validation\Rules\Enum;
+
+class RequestController extends Controller
+{
+    //
+    public function processe(Request $req, $reqId)
+    {
+        if (! Gate::allows('processe_req')) {
+            return back()
+                ->with(['error' => 'You\'re not allowed to process this request']);
+        }
+        try {
+            if (! BookRequest::where('id', $reqId)->exists()) {
+                return back()->with('error', 'Invalid request ID');
+            }
+
+            $status = $req->validate([
+                'status' => ['required', new Enum(RequestStatus::class)],
+            ]);
+
+            RequestInfo::create([
+                'user_id' => Auth::user()->id,
+                'request_id' => $reqId,
+                'status' => $status,
+            ]);
+
+            return back()
+                ->with(['message' => 'status updated successfully']);
+        } catch (\Throwable $th) {
+            return back()
+                ->with(['error' => 'Error while updating request']);
+        }
+
+    }
+}
