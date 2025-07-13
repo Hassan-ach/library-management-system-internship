ff2f7cc482cd2b69aa4d737306dec4c2c6752dab
Abdessalam <abdessalamaitoubanali@gmail.com>
2025-07-14 00:24:24 +0100

    feat(service): set updateBook and syncBook methods for BookService

--- app/Services/BookService.php
+++ app/Services/BookService.php
@@ -27,6 +27,25 @@ class BookService
         return $book;
     }
 
+    public function updateBook( int $id, string $title, string $isbn, string $description, string $publication_date, 
+                                int $pages, int $total_copies): Book{
+        // fetch the book from database by its ID
+        $book = Book::find( $id);
+
+        // update the book informations
+        $book->title = $title;
+        $book->isbn = $isbn;
+        $book->description = $description;
+        $book->publication_date = $publication_date;
+        $book->number_of_pages = $pages;
+        $book->total_copies = $total_copies;
+        
+        // save changes
+        $book->save();
+
+        return $book;
+    }
+
     public function attachBook( Book $book, array $tags, array $categories, array $authors, array $publishers): void{
         $book->tags()->attach( $tags);
         $book->categories()->attach( $categories);
@@ -34,4 +53,10 @@ class BookService
         $book->publishers()->attach( $publishers);
     }
 
+    public function syncBook( Book $book, array $tags, array $categories, array $authors, array $publishers): void{
+        $book->tags()->sync( $tags);
+        $book->categories()->sync( $categories);
+        $book->authors()->sync( $authors);
+        $book->publishers()->sync( $publishers);
+    }
 }
