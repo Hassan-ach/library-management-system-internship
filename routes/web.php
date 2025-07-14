0b797c7be7989265897601b362db2e9a48401440
ousam713 <ousambenz007@gmail.com>
2025-07-14 16:43:42 +0100

    add Admin Controllers

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminUserController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Auth\ProfileController;
@@ -24,3 +25,50 @@ Route::middleware(['auth:web', 'role:student'])->group(function () {
     Route::get('/books/search', [BookController::class, 'search'])->name('student.books.search');
     Route::get('/books', [BookController::class, 'index'])->name('student.books.list');
 });
+
+// <<<<<<<<<<<<<<<<<<<<<<<<<<< admin Routes
+
+Route::middleware(['auth:web','verified', 'role:student'])
+    ->group(function () {
+        
+
+    Route::get('/admin_users')->name('admin');
+
+    // gestion des utilisateurs (CRUD)
+    Route::prefix('admin')->name('admin.')->group(function() {
+
+        // <<<<<<<<<<<<<<<< users routes
+
+        // show all users
+        Route::get('/', [AdminUserController::class, 'users_list'])->name('index');
+
+        // search user
+        Route::get('/{user}', [AdminUserController::class, 'search'])->name('search.user');
+        
+
+        // <<<<<<<< create & store user
+        Route::get('/create', [AdminUserController::class, 'create_user'])->name('create.user');
+        Route::post('/', [AdminUserController::class, 'store.user'])->name('store.user');
+        // >>>>>>>> create & store user
+
+        // <<<<<<<< update & store user
+        Route::get('/{user}/edit', [AdminUserController::class, 'edit'])->name('edit.user');
+        Route::put('/{user}', [AdminUserController::class, 'update'])->name('update.user');
+        // >>>>>>>> update & store user
+
+        // <<<<<<<< delete user
+        Route::delete('/{user}', [AdminUserController::class, 'delete_user'])->name('delete.user');
+        // >>>>>>>> delete user
+
+
+        // >>>>>>>>>>>>>>>> users routes
+
+
+        // <<<<<<<<<<<<<<<< settings routes
+
+        // >>>>>>>>>>>>>>>> settings routes
+    });
+    
+});
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>> admin Routes
