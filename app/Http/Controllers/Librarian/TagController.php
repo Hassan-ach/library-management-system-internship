543f416427717c22851db78ef7ab20a9519cad63
Abdessalam <abdessalamaitoubanali@gmail.com>
2025-07-22 12:42:24 +0100

    add(controller): create an API to search existing tags

--- app/Http/Controllers/Librarian/TagController.php
+++ app/Http/Controllers/Librarian/TagController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Services\TagService;
 use Dotenv\Exception\ValidationException;
 use Illuminate\Http\Request;
+use App\Models\Tag;
 
 class TagController extends Controller
 {
@@ -97,6 +98,25 @@ class TagController extends Controller
         }
         catch(\Throwable $e){
             return view('errors.databaseException');
+        }
+    }
+    public function apiSearch(Request $request)
+    {
+        try {
+            $query = $request->input('q');
+            $tags = Tag::where('label', 'LIKE', "%{$query}%")
+                   ->limit(10)
+                   ->get();
+
+            return response()->json(
+                $tags->map( function($tag){
+                    return ['id'=>$tag->id, 'label'=>$tag->label];
+                })
+            );
+        } catch (\Throwable $th) {
+            //throw $th;
+
+            return response()->json([]);
         }
     }
 }
